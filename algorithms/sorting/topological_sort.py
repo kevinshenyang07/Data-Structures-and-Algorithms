25104711bcd86d346bad5d50656685ea94278429
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-08-22 17:21:25 -0700

    finish topological sort

--- algorithms/sorting/topological_sort.py
+++ algorithms/sorting/topological_sort.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+
+
 # Kahn's algorithm - repeatedly remove vertices that have no in edges
 # 1. queue up all vertices with no in edges
 # 2. Pop off vertices from the queue
@@ -17,22 +20,22 @@ def can_finish(num_courses, prerequisites):
     # prerequisites: array of [post, pre] course pairs
     # return: boolean
     # assumption: no duplicate edges
-    graph = { i: set() for i in range(num_courses) }
-    in_degrees = { i: 0 for i in range(num_courses) }
-    # for each course, create a prereq list
-    # for each course with prereq, add 1 in-degree
-    for post, pre in prerequisites:
-        graph[post].add(pre)
-        in_degrees[post] += 1
+    graph = { i: set() for i in range(num_courses) }  # course: corresponding post courses
+    in_degrees = { i: 0 for i in range(num_courses) }  # course: in-degree
+    for post_course, pre_course in prerequisites:
+        graph[pre_course].add(post_course)
+        in_degrees[post_course] += 1
     # vertices that have no in edges
     queue = [i for i in range(num_courses) if in_degrees[i] == 0]
     num_can_finish = 0
     while queue:
-        vertex = queue.pop(0)
+        course = queue.pop(0)
         num_can_finish += 1
-        for neighbor in graph[vertex]:
-            in_degrees[neighbor] -= 1
-            if in_degrees[neighbor] == 0:
-                queue.append(neighbor)
+        for post_course in graph[course]:
+            in_degrees[post_course] -= 1
+            # each time an in-edge is removed, if a vertex no longer has in-edges,
+            # add that vertex to check list
+            if in_degrees[post_course] == 0:
+                queue.append(post_course)
     return num_can_finish == num_courses
 # O(V+E) time and O(V+E) space
