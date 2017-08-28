25920e9e6b3fb63957c539a96422ee05ab7782e9
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-08-28 16:55:48 -0700

    implement generate parenthesis

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -159,6 +159,22 @@ def reverse(nums, left, right):
         right -= 1
 
 
+def generate_parenthesis(n):
+    def dfs(string, open_parens, close_parens):
+        # stop on invalid cases
+        if open_parens < close_parens:
+            return
+        # end on full length
+        if open_parens == n and close_parens == n:
+            result.append(string)
+        if open_parens <= n:
+            dfs(string + "(", open_parens + 1, close_parens)
+        if close_parens <= n:
+            dfs(string + ")", open_parens, close_parens + 1)
+    result = []
+    dfs("", 0, 0)
+    return result
+
 if __name__ == '__main__':
     nums = [3, 2, 1]
     print(subsets_tmp(nums))
