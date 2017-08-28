047d03fb0045c33b770a5e2e47778d8c708c76e2
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-08-28 00:16:11 -0700

    implement palindrome partitioning

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -2,17 +2,17 @@ from __future__ import print_function
 
 
 def subsets(nums):
-    results = [[]]
+    result = [[]]
     for num in sorted(nums):
-        results += [item + [num] for item in results]
-    return results
+        result += [item + [num] for item in result]
+    return result
 
 
 def subsets_dfs(nums):
     def dfs(depth, start, path):
-        # if nums contain duplicates, check if path in results
+        # if nums contain duplicates, check if path in result
         # before append
-        results.append(path)
+        result.append(path)
         if depth == len(nums):
             return
         # construct dfs calls by level
@@ -20,9 +20,26 @@ def subsets_dfs(nums):
             # later numbers get less options
             dfs(depth + 1, i + 1, path + [nums[i]])
     nums.sort()  # optional
-    results = []
+    result = []
     dfs(0, 0, [])
-    return results
+    return result
+
+
+def subsets_tmp(nums):
+    if not nums:
+        return []
+    result = [[]]
+    nums.sort()
+    subsets_helper(nums, 0, [], result)
+    return result
+
+
+def subsets_helper(nums, start, path, result):
+    result.append(path)
+    for i in range(start, len(nums)):
+        path.append(nums[i])
+        subsets_helper(nums, i + 1, path, result)
+        path.pop()
 
 
 def permute(nums):
@@ -42,22 +59,55 @@ def permute(nums):
 def permute_dfs(nums):
     def dfs(depth, candidates, path):
         if depth == len(nums):
-            results.append(path)  # only append full length permutation
+            result.append(path)  # only append full length permutation
             return
         for i in range(len(candidates)):
             # construct dfs calls by level
             dfs(depth + 1, candidates[:i] + candidates[i+1:], path + [candidates[i]])
     nums.sort()  # optional
-    results = []
+    result = []
     dfs(0, nums, [])
-    return results
+    return result
 
 
-def combination_sum():
-    pass
+# notes: 
+# 1. candidates has no duplicates, each number can be used multiple times
+# 2. all numbers in candidates and target are positive integers
+# 3. solution does not have duplicate combinations
+def combination_sum(candidates, target):
+    def dfs(start, path, target):
+        if target < 0:
+            return
+        if target == 0:
+            result.append(path)
+            return
+        for i in range(start, len(candidates)):
+            # keep adding one number until sum(path) > target
+            dfs(i, path + [candidates[i]], target - candidates[i])
+    candidates.sort()
+    result = []
+    dfs(0, [], target)
+    return result
+
+
+# Palindrome Partitioning
+def partition(s):
+    def dfs(sub_str, path):
+        if not sub_str:
+            result.append(path)
+            return
+        for i in range(1, len(sub_str) + 1):
+            if is_palindrome(sub_str[:i]):
+                dfs(s[i:], path + [s[:i]])
+    result = []
+    dfs(s, [])
+    return result
+
+def is_palindrome(s):
+    return s == s[::-1]
 
 
 if __name__ == '__main__':
     nums = [3, 2, 1]
-    print(subsets_dfs(nums))
+    print(subsets_tmp(nums))
     print(permute(nums))
