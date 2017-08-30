1c47ee9f20c7d3912a4fa3cc6c1518e6200ab1ed
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-08-30 12:46:21 -0700

    implement get_intersection_node

--- algorithms/linked_list/with_pointers.py
+++ algorithms/linked_list/with_pointers.py
@@ -23,3 +23,18 @@ def detect_cycle(head):
                 fast = fast.next
             return slow
     return None
+
+
+def get_intersection_node(head1, head2):
+    if not head1 or not head2:
+        return None
+    # get the tail of either list
+    tail1 = head1
+    while tail1.next:
+        tail1 = tail1.next
+    # make a cycle
+    tail1.next = head2
+    node_inter = detect_cycle(head1)
+    # cut cycle
+    tail1.next = None
+    return node_inter
