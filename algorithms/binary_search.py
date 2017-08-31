43d02a452162a8f6d850d2093b9f6b720b203af9
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-08-31 16:19:52 -0700

    transpile find median in sorted arrays solution

---
+++ algorithms/binary_search.py
@@ -0,0 +1,30 @@
+
+
+
+# Median of Two Sorted Arrays (hard)
+# do it in O(log(m+n)) time
+def find_median_sorted_arrays(nums1, nums2):
+    length = len(nums1) + len(nums2)
+    if length % 2 == 1:
+        return find_kth(nums1, nums2, length // 2)
+    else:
+        mid_left = find_kth(nums1, nums2, length // 2 - 1) 
+        mid_right = find_kth(nums1, nums2, length // 2)
+        return (mid_left + mid_right) / 2.0
+
+
+# each call cuts the shorter array by half of its size
+def find_kth(nums1, nums2, k):
+    if len(nums1) > len(nums2):
+        nums1, nums2 = nums2, nums1
+    if not nums1:
+        return nums2[k]
+    if k == len(nums1) + len(nums2) - 1:
+        return max(nums1[-1], nums2[-1])
+    i = len(nums1) // 2
+    j = k - i
+    if nums1[i] > nums2[j]:
+        return find_kth(nums1[:i], nums2[j:], i)
+    else:
+        return find_kth(nums1[i:], nums2[:j], j)
+
