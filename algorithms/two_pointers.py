3fc159e9bac4107c3a1f7bd10c26132064e8b335
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-09-03 20:14:55 -0700

    implement n-sum series

--- algorithms/two_pointers.py
+++ algorithms/two_pointers.py
@@ -1,3 +1,47 @@
+# Two Sum - Sorted Array
+def two_sum2(nums, target):
+    start, end = 0, len(nums) - 1
+    while start < end:
+        if nums[start] + nums[end] == target:
+            return [start + 1, end + 1]
+        elif nums[start] + nums[end] < target:
+            start += 1
+        else:
+            end -= 1
+    return []
+# O(n) time, O(1) space
+
+
+# Two Sum - Closet to Target
+
+# Two Sum - Difference Equals to Target
+
+
+# 3Sum (same as Two Sum, use hashmap or sort+pointers)
+def three_sum(nums):
+    # sum to 0
+    result = []
+    nums.sort()
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i-1] == nums[i]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+            if s < 0:
+                l += 1
+            elif s > 0:
+                r -= 1
+            else:
+                result.append([nums[i], nums[l], nums[r]])
+                while l < r and nums[l] == nums[l+1]:
+                    l += 1
+                while l < r and nums[r-1] == nums[r]:
+                    r -= 1
+                l += 1
+                r -= 1
+    return result
+
 # Merge Sorted Array
 # assumption: nums1 has extra space to hold all the elements in nums
 # m and n are the number of elements in these two static arrays
@@ -17,3 +61,18 @@ def merge(nums1, m, nums2, n):
         for i in range(n):
             nums1[i] = nums2[i]
         
+
+# move zeros to the end of array, do it in-place
+def move_zeros(nums):
+    if len(nums) <= 1:
+        return
+    # find the initial index of zero
+    p0 = 0
+    while p0 < len(nums) and nums[p0] != 0:
+        p0 += 1
+    # start at the index after zero pointer
+    for i in range(p0 + 1, len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[p0] = nums[p0], nums[i]
+            # nums[p0+1] is guaranteed to be 0
+            p0 += 1
