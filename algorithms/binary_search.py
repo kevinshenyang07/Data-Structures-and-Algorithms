f1419adf1e8c83e591394eb0a29dde421f445bc0
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-09-04 17:06:25 -0700

    implement sprt(x)

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -20,6 +20,24 @@ def my_pow(x, n):
         x = x * x
         n = n // 2
     return res
+
+
+def my_sqrt(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    if x <= 1:
+        return x
+    l, r = 0, x
+    while l + 1 < r:
+        mid = l + (r - l) // 2
+        if mid * mid <= x < (mid + 1) * (mid + 1):
+            return mid
+        elif mid * mid > x:
+            r = mid
+        else:
+            l = mid
     
 
 # Median of Two Sorted Arrays (hard but classic)
