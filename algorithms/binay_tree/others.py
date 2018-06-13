dd826349c895ff1c8ccf38fe31c421fdc6ee5f35
Yang Shen <kevinshenyang07@gmail.com>
2018-06-13 15:52:02 -0700

    rm

--- algorithms/binay_tree/others.py
+++ algorithms/binay_tree/others.py
@@ -1,5 +1,7 @@
 from tree_node import TreeNode
 
+# NOTE: 许多题见模板templates.py
+
 # divide & conquer version
 def depth(root):
     if not root:
@@ -37,44 +39,6 @@ def is_balanced(root):
     return is_balanced(root.left) and is_balanced(root.right)
 
 
-def inorder_rec(root, arr=[]):
-    if root:
-        inorder_rec(root.left, arr)
-        arr.append(root.val)
-        inorder_rec(root.right, arr)
-    return arr
-
-
-def inorder(root):
-    stack = []
-    visited = []
-    while root or stack:
-        while root:
-            stack.append(root)
-            root = root.left
-        root = stack.pop()
-        visited.append(root.val)
-        root = root.right
-    return visited
-
-
-def is_valid_bst(root):
-    if not root:
-        return True
-    stack = []
-    prev, curr = None, root
-    while curr or stack:
-        while curr:
-            stack.append(curr)
-            curr = curr.left
-        curr = stack.pop()
-        if prev and prev.val > curr.val:
-            return False
-        prev = curr
-        curr = curr.right
-    return True
-
-
 # Construct Binary Tree from Preorder and Inorder Traversal
 def build_tree(preorder, inorder):
     # preorder and inorder are lists of node values
@@ -98,24 +62,6 @@ def has_path_sum(root, target):
         return True
     target -= root.val
     return has_path_sum(root.left, target) or has_path_sum(root.right, target)
-
-
-def LCA(root, node1, node2):
-    # base cases
-    if root is None or root == node1 or root == node2:
-        return root
-    # divide
-    left = LCA(root.left, node1, node2)
-    right = LCA(root.right, node1, node2)
-    # conquer
-    # if node1 and node2 are in different subtrees
-    if left and right:
-        return root
-    if left:
-        return left
-    if right:
-        return right
-    return None
 
 
 # Populating Next Right Pointers in Each Node
