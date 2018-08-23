39e370182c0c6afc726381a085b9687b0119ba55
Yang Shen <kevinshenyang07@gmail.com>
2018-08-22 23:11:28 -0700

    rename

---
+++ algorithms/dynamic_programming/longest_increasing_subsequence.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+
+# Longest Increasing Subsequence
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # tails[i] = the smallest tail of all increasing subseq with length i+1
+        # for example give [4, 5, 6, 3],
+        # tails[1] = 5 (from [4, 5], [5, 6])
+        tails = [0] * len(nums)
+        size = 0
+
+        for x in nums:
+            i, j = 0, size
+
+            # find the smallest tail for tails[i]
+            while i < j:
+                m = (i + j) / 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+
+            tails[i] = x
+            size = max(size, i + 1)  # if x is larger than all tails
+
+        return size
+
+
+if __name__ == '__main__':
+    seq = [2, 1, 5, 3, 6, 4, 8, 9, 7]
+    print(Solution().lengthOfLIS(seq))
