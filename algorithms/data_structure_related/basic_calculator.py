3afb35c79de019087603968b87455b8e43a86953
Yang Shen <kevinshenyang07@gmail.com>
2018-09-07 19:19:38 -0700

    basic calc ii

--- algorithms/data_structure_related/basic_calculator.py
+++ algorithms/data_structure_related/basic_calculator.py
@@ -1,6 +1,4 @@
 # Basic Calculator
-import string
-
 # assumption: the expression is valid, integers are non-negative
 # may have white space
 class Solution(object):
@@ -17,7 +15,7 @@ class Solution(object):
                 stack[-1].append(num)
                 num = 0
                 stack[-1].append(char)
-            elif char in string.digits:
+            elif char.isdigit():
                 num = 10 * num + int(char)
 
         stack[-1].append(num)  # add last number
@@ -34,3 +32,42 @@ class Solution(object):
                 sign = 1 if element == '+' else -1
 
         return ans
+
+
+# Basic Calculator II
+# assumption: the expression is valid, there won't be expr like 5/-2
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = []  # stack[-1]: a
+        operator = '+'
+        num = 0  # b
+
+        for i, char in enumerate(s):
+            if char == ' ':
+                continue
+            elif char in '+-*/':
+                operator = char
+            else:
+                num = 10 * num + int(char)
+                if i == len(s) - 1 or not s[i + 1].isdigit():
+                    if operator == '+':
+                        stack.append(num)
+                    elif operator == '-':
+                        stack.append(-num)
+                    elif operator == '*':
+                        stack.append(stack.pop() * num)
+                    elif operator == '/':
+                        # divide negative number gets rounded down: -4 / 3 == -2
+                        a = stack.pop()
+                        sign = 1 if a >= 0 else -1
+                        stack.append(sign * (abs(a) / num))
+                    num = 0
+
+        res = 0
+        while stack:
+            res += stack.pop()
+        return res
