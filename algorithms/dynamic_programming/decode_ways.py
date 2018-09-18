82962cd58a8299c29c29d486332c51a896bd9d06
Yang Shen <kevinshenyang07@gmail.com>
2018-09-17 20:37:29 -0700

    decode ways

--- algorithms/dynamic_programming/decode_ways.py
+++ algorithms/dynamic_programming/decode_ways.py
@@ -4,15 +4,17 @@ def num_decodings(s):
     #dp[i] = 0 if s[i-1] == "0"
     #      + dp[i-1] if s[i-1] != "0"
     #      + dp[i-2] if "09" < s[i-2:i] < "27"
-    if s == "": return 0
+    if not s or s[0] == '0': return 0
 
-    dp = [0 for x in range(len(s)+1)]
+    dp = [0] * (len(s) + 1)
     dp[0] = 1
+    dp[1] = 1
 
-    for i in range(1, len(s)+1):
+    for i in range(2, len(s)+1):
         if s[i-1] != "0":
             dp[i] += dp[i-1]
-        if i != 1 and s[i-2] != "0" and int(s[i-2:i]) <= 26:
+        if s[i-2] != "0" and int(s[i-2:i]) <= 26:
             dp[i] += dp[i-2]
+
     return dp[-1]
 # dp can be optimized to two variables
