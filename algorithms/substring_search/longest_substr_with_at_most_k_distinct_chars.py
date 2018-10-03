2bfab4f69dd9e56103d933d1efdaaf54b872984a
Yang Shen <kevinshenyang07@gmail.com>
2018-10-02 21:52:01 -0700

    longest substr

---
+++ algorithms/substring_search/longest_substr_with_at_most_k_distinct_chars.py
@@ -0,0 +1,23 @@
+# Longest Substring with At Most K Distinct Characters
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        max_length = 0
+        left = 0  # [left, i] is the current valid window
+        mapping = {}  # char => largest index of this char appears so far
+
+        for i, char in enumerate(s):
+            mapping[char] = i
+
+            if len(mapping) > k:
+                j = min(mapping.values())
+                del mapping[s[j]]
+                left = j + 1
+
+            max_length = max(max_length, i - left + 1)
+
+        return max_length
