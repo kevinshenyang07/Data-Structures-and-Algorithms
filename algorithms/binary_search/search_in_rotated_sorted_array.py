2d49720ec2d166bcef25ffa0eb9529d142398b91
Yang Shen <kevinshenyang07@gmail.com>
2018-10-06 16:35:00 -0700

    search sorted

--- algorithms/binary_search/search_in_rotated_sorted_array.py
+++ algorithms/binary_search/search_in_rotated_sorted_array.py
@@ -3,26 +3,29 @@
 def search_rotated(nums, target):
     if not nums:  # empty list or None
         return -1
-    start, end = 0, len(nums) - 1
-    while start + 1 < end:  # make sure start and mid don't meet
-        mid = start + (end - start) // 2
-        # two cases * two conditions
+
+    left, right = 0, len(nums) - 1
+
+    while left + 1 < right:
+        mid = left + (right - left) // 2
         if nums[mid] == target:
             return mid
-        if nums[start] < nums[mid]:  # mid is on the left half
-            # find the condition that divide possible solution range
-            if nums[start] <= target <= nums[mid]:
-                end = mid
-            else:  # other wise cut from start to mid
-                start = mid
-        else:
-            if nums[mid] <= target <= nums[end]:
-                start = mid
+        # length of rotated part < (right - left) / 2
+        if nums[left] < nums[mid]:
+            # continuous range on the left
+            if nums[left] <= target <= nums[mid]:
+                right = mid
             else:
-                end = mid
-    # handle the cases when start = mid
-    if nums[start] == target:
-        return start
-    if nums[end] == target:
-        return end
+                left = mid
+        else:
+            # continuous range on the right
+            if nums[mid] <= target <= nums[right]:
+                left = mid
+            else:
+                right = mid
+    # handle boundary cases
+    if nums[left] == target:
+        return left
+    if nums[right] == target:
+        return right
     return -1
