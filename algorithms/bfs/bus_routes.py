d49749f08f0ac76bc5c03c7b55b82b1cd29a5c68
Yang Shen <kevinshenyang07@gmail.com>
2018-10-08 23:10:23 -0700

    bus routes

---
+++ algorithms/bfs/bus_routes.py
@@ -0,0 +1,41 @@
+from collections import defaultdict, deque
+
+# Bus Routes
+# each routes[i] is a bus route that the i-th bus repeats forever
+# we start at bus stop S and we want to go to bus stop T
+# rravelling by buses only, find the least number of buses to take
+class Solution(object):
+    def numBusesToDestination(self, routes, S, T):
+        """
+        :type routes: List[List[int]]
+        :type S: int
+        :type T: int
+        :rtype: int
+        """
+        stop_to_routes = defaultdict(set)
+
+        for i, route in enumerate(routes):
+            for stop in route:
+                stop_to_routes[stop].add(i)
+
+        visited_stops = set([S])
+        visited_routes = set()  # further reduce time complexity
+        queue = deque([(S, 0)])  # if S == T , no need to take a bus
+
+        while queue:
+            stop, num_bus = queue.popleft()
+
+            if stop == T:
+                return num_bus
+
+            for route_id in stop_to_routes[stop]:
+                if route_id not in visited_routes:
+
+                    for next_stop in routes[route_id]:
+                        if next_stop not in visited_stops:
+                            queue.append((next_stop, num_bus + 1))
+                            visited_stops.add(next_stop)
+
+                    visited_routes.add(route_id)
+
+        return -1
