e297fe884684779caf01c6ac77585af820f8d77a
Yang Shen <kevinshenyang07@gmail.com>
2018-10-10 00:06:53 -0700

    k empty slots

---
+++ algorithms/others/k_empty_slots.py
@@ -0,0 +1,39 @@
+import math
+
+# K Empty Slots
+# brute force: on each day, when a flower blooms, look k distance back / forward
+# which is O(nk) time, O(n) space
+class Solution(object):
+    def kEmptySlots(self, flowers, k):
+        """
+        :type flowers: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if not flowers:
+            return -1
+
+        # partition slots into buckets with size to be k + 1
+        # so that even the max - min element in one bucket would not qualify
+        # range of bucket[0]: [1, k + 1], bucket[1]: [k + 2, 2k + 2] ...
+        num_buckets = int(math.ceil(len(flowers) / (k + 1.0)))
+        bucket_mins = [float('inf')] * num_buckets
+        bucket_maxs = [float('-inf')] * num_buckets
+
+        for i, pos in enumerate(flowers):
+            # update the range of corresponding bucket on the go
+            j = (pos - 1) / (k + 1)
+            if pos <= bucket_mins[j]:
+                bucket_mins[j] = pos
+                # compare with left max
+                if j > 0 and bucket_mins[j] - bucket_maxs[j - 1] == k + 1:
+                    return i + 1
+            if pos >= bucket_maxs[j]:
+                bucket_maxs[j] = pos
+                # compare with right min
+                if j < num_buckets - 1 and bucket_mins[j + 1] - bucket_maxs[j] == k + 1:
+                    return i + 1
+            # if pos is between min and max, there won't be an answer for that flower
+
+        return -1
+# O(n) time, O(n/(k+1)) space
