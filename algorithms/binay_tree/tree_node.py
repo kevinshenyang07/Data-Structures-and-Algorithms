e53574cd08a5e83817cce097106a91d4147805f0
Yang Shen <kevinshenyang07@gmail.com>
2018-10-19 15:25:00 -0700

    tree node

--- algorithms/binay_tree/tree_node.py
+++ algorithms/binay_tree/tree_node.py
@@ -5,9 +5,15 @@ class TreeNode(object):
         self.right = None
 
 
-class TreeLinkNode:
+class TreeLinkNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
         self.next = None
+
+
+class TreeNaryNode(object):
+    def __init__(self, x, childlren):
+        self.val = x
+        self.childlren = childlren
