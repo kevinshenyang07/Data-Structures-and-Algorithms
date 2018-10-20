33327445e47863e7fb067eace279e8b883e4b1e8
Yang Shen <kevinshenyang07@gmail.com>
2018-10-20 15:38:48 -0700

    heap and related

---
+++ algorithms/heap/heap.py
@@ -0,0 +1,20 @@
+from heapq import heappush, heappop
+
+# heap with key
+class Heap(object):
+    def __init__(self, key=lambda x: x):
+        self.key = key
+        self.data = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def push(self, x):
+        heappush(self.data, (self.key(x), x))
+
+    def pop(self):
+        return heappop(self.data)[1]
+
+    def peek(self):
+        if self.data:
+            return self.data[0][1]
