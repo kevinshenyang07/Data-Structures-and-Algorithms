493d9003668d286e877a7ff8fafc7a6cca3fcad1
Yang Shen <kevinshenyang07@gmail.com>
2018-10-30 00:06:05 -0700

    spiral matrix ii

--- algorithms/others/spiral_matrix.py
+++ algorithms/others/spiral_matrix.py
@@ -1,6 +1,6 @@
 # Spiral Matrix
 class Solution(object):
-    def spiral_order(self, matrix):
+    def spiralOrder(self, matrix):
         if not any(matrix):
             return []
 
@@ -10,24 +10,53 @@ class Solution(object):
         result = []
 
         while True:
-            for i in range(left, right + 1):
-                result.append(matrix[top][i])
+            for j in range(left, right + 1):
+                result.append(matrix[top][j])
             top += 1
-            if left > right or top > bottom: break;
+            if top > bottom or left > right:
+                break
 
             for i in range(top, bottom + 1):
                 result.append(matrix[i][right])
             right -= 1
-            if left > right or top > bottom: break;
+            if top > bottom or left > right:
+                break
 
-            for i in range(right, left - 1, -1):
-                result.append(matrix[bottom][i])
+            for j in range(right, left - 1, -1):
+                result.append(matrix[bottom][j])
             bottom -= 1
-            if left > right or top > bottom: break;
+            if top > bottom or left > right:
+                break
 
             for i in range(bottom, top - 1, -1):
                 result.append(matrix[i][left])
             left += 1
-            if left > right or top > bottom: break;
+            if top > bottom or left > right:
+                break
 
         return result
+
+
+# Spiral Matrix II
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        matrix = [[0] * n for _ in range(n)]
+
+        x = y = 0
+        dx, dy = 0, 1
+
+        for i in range(1, n * n + 1):
+            matrix[x][y] = i
+
+            # turns right if next position is visited
+            if matrix[(x + dx) % n][(y + dy) % n]:
+                dx, dy = dy, -dx
+
+            x += dx
+            y += dy
+
+        return matrix
