32917deaaf989fd31e78fc5deb7a5244021d6a08
Yang Shen <kevinshenyang07@gmail.com>
2018-11-02 17:42:13 -0700

    quick select

--- algorithms/sorting/quick_select.py
+++ algorithms/sorting/quick_select.py
@@ -2,45 +2,46 @@
 def find_kth_largest(nums, k):
     if not nums or k <= 0 or k > len(nums):
         return 0
-    return select(nums, 0, len(nums) - 1, len(nums) - k)
+    return quick_select(nums, 0, len(nums) - 1, len(nums) - k)
 
 # find kth smallest in range
-def select(nums, start, end ,k):
-    if start == end:
-        return nums[start]
+def quick_select(nums, i, j ,k):
+    if i == j:
+        return nums[i]
 
-    pivot = partition(nums, start, end)
-    # only partition the range that contains k
-    if pivot == k:
-        return nums[pivot]
-    elif pivot < k:
-        return select(nums, pivot + 1, end, k)
+    pivot = partition(nums, i, j)
+    # narrow the partition range by pivot index
+    if pivot < k:
+        return quick_select(nums, pivot + 1, j, k)
+    elif pivot > k:
+        return quick_select(nums, i, pivot - 1, k)
     else:
-        return select(nums, start, pivot - 1, k)
+        return nums[pivot]
 
 # partition with two pointers, returns a partition index where
-# every element on the left is <= nums[i]
+# every element on the left is <= nums[i] (pivot value)
 # every element on the right is >= nums[i]
-def partition(nums, start, end):
+def partition(nums, i, j):
     # if using random init pivot, swap to the left anyway
-    pivot_val = nums[start]
-    left = start + 1
-    right = end
-    # let right cross left to make sure nums[right] < pivot_val in the end
+    left = i + 1
+    right = j
+    # let right cross left to make sure nums[right] < nums[i] in the end
     # left equal to right covers the two elements situation
     while left <= right:
-        if nums[left] <= pivot_val:
+        if nums[left] <= nums[i]:
             left += 1
-        elif nums[right] >= pivot_val:
+        elif nums[right] >= nums[i]:
             right -= 1
         # if two pointers haven't crossed yet and ready to swap
         else:
             nums[left], nums[right] = nums[right], nums[left]
     # swap pivot with the rightmost element that is smaller than pivot
-    nums[start], nums[right] = nums[right], nums[start]
+    nums[i], nums[right] = nums[right], nums[i]
     return right
-
-
 # O(n) time, O(n^2) worst case, O(1) space
 # the first call of partition takes O(n), the next take an average of O(n/2)...
 # worst case happens on selecting maximum on a sorted list, using 0 as pivot index
+
+if __name__ == '__main__':
+    nums = [10, 4, 5, 8, 6, 11, 26]
+    print find_kth_largest(nums, 4)  # 8
