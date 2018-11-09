657d8a1f0d016a10b8820d96d3d5bbd5797148e6
Yang Shen <kevinshenyang07@gmail.com>
2018-11-09 11:52:16 -0800

    boundary of binary tree

--- algorithms/binay_tree/boundary_of_binary_tree.py
+++ algorithms/binay_tree/boundary_of_binary_tree.py
@@ -15,46 +15,43 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        if not root: return []
+        if not root:
+            return []
+        # start from root.left/.right to avoid duplicates
+        self.boundary = [root.val]
+        self.left_bound(root.left)
+        self.leaves(root.left)
+        self.leaves(root.right)
+        self.right_bound(root.right)
 
-        boundary = [root.val]
-        self.left_bound(root.left, boundary)
-        self.leaves(root.left, boundary)
-        self.leaves(root.right, boundary)
-        self.right_bound(root.right, boundary)
-
-        return boundary
+        return self.boundary
 
     # preorder
-    def left_bound(self, root, boundary):
+    def left_bound(self, root):
         if not root or (not root.left and not root.right):
             return
-        boundary.append(root.val)
+        self.boundary.append(root.val)
         if root.left:
-            self.left_bound(root.left, boundary)
+            self.left_bound(root.left)
         else:
-            self.left_bound(root.right, boundary)
+            self.left_bound(root.right)
 
     # postorder
-    def right_bound(self, root, boundary):
+    def right_bound(self, root):
         if not root or (not root.left and not root.right):
             return
         if root.right:
-            self.right_bound(root.right, boundary)
+            self.right_bound(root.right)
         else:
-            self.right_bound(root.left, boundary)
-        boundary.append(root.val)
+            self.right_bound(root.left)
+        self.boundary.append(root.val)
 
-    # DFS
-    # level order does not work for get leaves
-    # since it add leaves not at the last level first，
-    # which should actually be the last
-    def leaves(self, root, boundary):
+    # DFS (level order does not work for get leaves)
+    def leaves(self, root):
         if not root:
             return
         if not root.left and not root.right:
-            boundary.append(root.val)
+            self.boundary.append(root.val)
             return
-        self.leaves(root.left, boundary)
-        self.leaves(root.right, boundary)
-
+        self.leaves(root.left)
+        self.leaves(root.right)
