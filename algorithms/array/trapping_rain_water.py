3849d8a1165387d961c82aa4970d5a8dd3ad01f6
Yang Shen <kevinshenyang07@gmail.com>
2018-11-09 16:59:44 -0800

    trapping rain water

--- algorithms/array/trapping_rain_water.py
+++ algorithms/array/trapping_rain_water.py
@@ -1,27 +1,30 @@
 # Trapping Rain Water
+# both ends cannot contain water
 def trap(height):
     """
     :type height: List[int]
     :rtype: int
     """
-    if len(height) <= 2:
-        return 0
+    n = len(height)
+    left = [0] * n  # left[i] =  max(height[:i])
+    right = [0] * n  # right[i] = max(height[:i+1])
 
-    # highest[i] = min(max(height[:i]), max(height[:i+1]))
-    highest = [0] * len(height)
+    # find highest bar looking left
+    for i in range(n):
+        if i == 0:
+            left[i] = height[i]
+        else:
+            left[i] = max(left[i - 1], height[i])
 
-    # both ends cannot contain water
-    # find highest bar look left
-    for i in range(1, len(height) - 1):
-        highest[i] = max(highest[i - 1], height[i - 1])
-    # find highest bar look right
-    for i in range(len(height) - 2, -1, -1):
-        highest_right = max(highest[i + 1], height[i + 1])
-        highest[i] = min(highest[i], highest_right)
+    # find highest bar looking right
+    for i in range(n - 1, -1, -1):
+        if i == n - 1:
+            right[i] = height[i]
+        else:
+            right[i] = max(right[i + 1], height[i])
 
-    vol = 0
-    for i in range(1, len(height) - 1):
-        if highest[i] > height[i]:
-            vol += highest[i] - height[i]  # volume trapped on each slot
-    return vol
+    volume = 0
+    for i in range(n):
+        volume += min(left[i], right[i]) - height[i]  # volume trapped on each slot
+    return volume
 # O(n) time and space
