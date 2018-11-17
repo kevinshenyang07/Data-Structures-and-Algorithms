7c75644b3dbc7b5d760e3b6074c2bdca718f63b5
Yang Shen <kevinshenyang07@gmail.com>
2018-11-16 21:23:14 -0800

    largest font

--- algorithms/google/largest_font_fit_sentence_on_screen.py
+++ algorithms/google/largest_font_fit_sentence_on_screen.py
@@ -4,44 +4,72 @@
 # 可以将屏幕按照font的宽高换算成有多少rows * cols，然后用Sentence Screen Fitting的思路判断是否能装下一个字符串或者句子。
 # 上面这个作为binary search的条件，最终锁定满足条件的最大字体。（应该是返回right index）
 class Solution(object):
-    def get_largest_font(self, s, height, width, font_lo, font_hi):
+    def get_largest_font(self, s, H, W, font_lo, font_hi):
         """
         s: sentence
-        height: screen height
-        width: screen width
+        H: screen height
+        W: screen width
         font_lo: smallest possible font
         font_hi: largest possible font
         """
         while font_lo + 1 < font_hi:
             font_mid = font_lo + (font_hi - font_lo) / 2
-            if self.can_fit(s, height, width, font_mid):
+            if self.can_fit(s, H, W, font_mid):
                 font_lo = font_mid
             else:
                 font_hi = font_mid
 
-        if self.can_fit(s, height, width, font_hi):
+        if self.can_fit(s, H, W, font_hi):
             return font_hi
-        elif self.can_fit(s, height, width, font_lo):
+        elif self.can_fit(s, H, W, font_lo):
             return font_lo
         else:
             return -1
 
-    # string can be split into multiple rows
-    def can_fit(self, s, height, width, font):
-        total_rows = height / getHeight(font)
-        i = 0
+    # a word can be split into multiple rows
+    def can_fit(self, s, H, W, font):
+        total_rows = H / getHeight(font)
         rows = 1
-        curr_width = 0
+        width = 0
 
-        for i, char in enumerate(s):
+        for char in s:
             w = getWidth(char, font)
-            curr_width += w
+            width += w
 
-            if curr_width > width:
+            if width > W:
+                width = w
                 rows += 1
-                curr_width = w
 
             if rows > total_rows:
                 return False
+
+        return True
+
+    # a word cannot be split into multiple rows
+    def can_fit(self, s, H, W, font):
+        total_rows = H / getHeight(font)
+        rows = 1
+        width = 0
+        i = 0
+        words = s.split(' ')
+
+        while i < len(words):
+            w = sum(getWidth(char, font) for char in words[i])
+            width += w
+
+            if width + getWidth(' ', font) <= W:
+                width += getWidth(' ', font)
+
+            if width > W:
+                width = w
+                rows += 1
+
+            # a word that cannot fit into any rows
+            if w > W:
+                return False
+            if rows > total_rows:
+                return False
+
+            i += 1
 
         return True
