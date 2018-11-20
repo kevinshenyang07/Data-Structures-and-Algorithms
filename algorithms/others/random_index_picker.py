0db9dc49574f642a02ad1fce595664a33cc907eb
Yang Shen <kevinshenyang07@gmail.com>
2018-11-19 19:55:38 -0800

    random index picker

--- algorithms/others/random_index_picker.py
+++ algorithms/others/random_index_picker.py
@@ -15,15 +15,14 @@ class RandomIndexPicker(object):
         for i, num in enumerate(self.nums):
             if num == target:
                 count += 1
-                # 1 / count > random
-                if (1.0 / count) > random.random():
+                if 1.0 / count > random.random():
                     picked = i
         return picked
 # O(n) time, O(1) space
 # proof:
 # say there're k numbers equal to target, 1 <= i <= k
 # p(picked == i) = (1 / i)            p(initially picked i)
-#                * (i / i + 1 )       p(then i + 1 not picked)
+#                * (i / i + 1)        p(then i + 1 not picked)
 #                * (i + 1 / i + 2)    p(then i + 2 not picked)
 #                * ...
 #                * (k - 2 / k - 1)    p(then k - 1 not picked)
@@ -33,3 +32,26 @@ class RandomIndexPicker(object):
 # say we have 4 '1's  in the array, the first index with value '1' has p == 1 to be picked,
 # the second has p == 1 / 2, the third has p == 1 / 3, the last has p == 1 / 4
 # p(picked == 2) = (1 / 2) * (2 / 3) * (3 / 4)
+
+
+# randomly smaple k elements
+class RandomSampler(object):
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def sample(self, k):
+        reservoir = nums[:k]
+        for i, num in enumerate(nums):
+            j = random.randint(0, i)
+            if j < k:
+                reservoir[j] = i
+        return reservoir
+# O(n) time, O(1) extra space
+# p(nums[0] in reservoir) = (1 / 1)
+#                         * (1 / 2)
+#                         * (2 / 3)
+#                         * ...
+#                         = (k - 2 / k -1)
+#                         = (k - 1 / k)
+#                         = 1 / k
