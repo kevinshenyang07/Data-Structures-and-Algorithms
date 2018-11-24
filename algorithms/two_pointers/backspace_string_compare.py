ad0b530b95834dc5f4c1dc5b772722bb40a596a5
Yang Shen <kevinshenyang07@gmail.com>
2018-11-24 15:01:50 -0800

    backspace strign compare

---
+++ algorithms/two_pointers/backspace_string_compare.py
@@ -0,0 +1,41 @@
+# Backspace String Compare
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        i, j = len(S) - 1, len(T) - 1
+        back_s = back_t = 0
+
+        while i >= 0 or j >= 0:
+            while i >= 0 and (back_s or S[i] == '#'):
+                if S[i] == '#':
+                    back_s += 1
+                else:
+                    back_s -= 1
+                i -= 1
+
+            while j >= 0 and (back_t or T[j] == '#'):
+                if T[j] == '#':
+                    back_t += 1
+                else:
+                    back_t -= 1
+                j -= 1
+
+            if i >= 0 and j >= 0 and S[i] != T[j]:
+                return False
+            # still remaining chars in S or T after inner while loops
+            if i * j < 0:
+                return False
+
+            i -= 1
+            j -= 1
+
+        return i == j
+# O(n) time, O(1) space
+# test case:
+# 1. "nzp#o#g", "b#nzp#o#g"  => true
+# 2. "a##c", "#a#c" => true
+# 3. "a#c", "b" => false
