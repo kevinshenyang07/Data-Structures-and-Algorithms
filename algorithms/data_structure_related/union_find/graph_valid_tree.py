2bdb1928f1d2e9db018ff73c7c256b1a72dd7bab
Yang Shen <kevinshenyang07@gmail.com>
2018-11-28 14:30:52 -0800

    detect cycle

--- algorithms/data_structure_related/union_find/graph_valid_tree.py
+++ algorithms/data_structure_related/union_find/graph_valid_tree.py
@@ -18,3 +18,53 @@ class Solution(object):
             uf.union(p, q)
         # no forest
         return uf.num_groups == 1
+
+
+# DFS
+class Solution(object):
+    def validTree(self, n, edges):
+        graph = { i: set() for i in range(n) }
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        self.graph = graph
+        self.visited = set()
+        return self.dfs(0) and len(self.visited) == n
+
+    def dfs(self, u):
+        # should have no cycle
+        if len(self.visited & self.graph[u]) >= 2:
+            return False
+
+        self.visited.add(u)
+
+        for v in self.graph[u]:
+            if v not in self.visited and not self.dfs(v):
+                return False
+        return True
+
+
+# BFS
+class Solution(object):
+    def validTree(self, n, edges):
+        graph = { i: set() for i in range(n) }
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        queue = collections.deque([0])
+        visited = set([0])
+
+        while queue:
+            u = queue.popleft()
+            # should have no cycle
+            if len(visited & graph[u]) >= 2:
+                return False
+
+            for v in graph[u]:
+                if v not in visited:
+                    queue.append(v)
+                    visited.add(v)
+
+        return len(visited) == n
